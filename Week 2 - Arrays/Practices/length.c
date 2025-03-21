4b9dfcf1f198027b0bd2061df46eadaad0b49ff3
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-21 22:58:33 +0100

    Update length.c

--- Week 2 - Arrays/Practices/length.c
+++ Week 2 - Arrays/Practices/length.c
@@ -1,22 +1,32 @@
+// This program prompts the user for their name and then calculates and displays the length of the provided name.
+
 #include <stdio.h>
 #include <cs50.h>
 
+// Function prototype for calculating the length of a string
 int string_length(string name);
 
 int main(void)
 {
+    // Prompt user for their name
     string name = get_string("Name: ");
+    
+    // Calculate the length of the provided name
     int length = string_length(name);
 
+    // Print the length of the name
     printf("The length of your name is %i\n", length);
 }
 
+// Function to calculate the length of a string
 int string_length(string name)
 {
     int n = 0;
+    // Iterate through each character in the string until the null terminator is reached
     while(name[n] != '\0')
     {
         n++;
     }
+    // Return the length of the string
     return n;
 }
