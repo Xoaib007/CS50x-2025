baab002a47e81c41dfe3240172c49418fe76381b
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-21 22:42:36 +0100

    Update cash.c

--- Week 1 - C/Problem Set - 1/Solutions/cash.c
+++ Week 1 - C/Problem Set - 1/Solutions/cash.c
@@ -1,6 +1,10 @@
+// Program to calculate the minimum number of coins required for a given amount of cents.
+// It uses functions to determine the number of quarters, dimes, nickels, and pennies needed.
+
 #include <cs50.h>
 #include <stdio.h>
 
+// Function prototypes
 int get_cents(void);
 int quarter_amount(int cents);
 int dime_amount(int cents);
@@ -9,35 +13,43 @@ int penny_amount(int cents);
 
 int main(void)
 {
+    // Get the amount of cents owed
     int cents = get_cents();
 
+    // Calculate the number of quarters
     int quarters = quarter_amount(cents);
     cents = cents - quarters * 25;
 
+    // Calculate the number of dimes
     int dimes = dime_amount(cents);
     cents = cents - dimes * 10;
 
+    // Calculate the number of nickels
     int nickels = nickel_amount(cents);
     cents = cents - nickels * 5;
 
+    // Calculate the number of pennies
     int pennies = penny_amount(cents);
     cents = cents - pennies * 1;
 
+    // Calculate the total number of coins
     int totalCoins = quarters + dimes + nickels + pennies;
     printf("%i\n", totalCoins);
 }
 
+// Function to get the amount of cents owed from the user
 int get_cents(void)
 {
     int n;
     do
     {
-        n = get_int("changed owed:");
+        n = get_int("change owed: ");
     }
     while (n < 1);
     return n;
 }
 
+// Function to calculate the number of quarters
 int quarter_amount(int cents)
 {
     int count = 0;
@@ -49,6 +61,7 @@ int quarter_amount(int cents)
     return count;
 }
 
+// Function to calculate the number of dimes
 int dime_amount(int cents)
 {
     int count = 0;
@@ -60,6 +73,7 @@ int dime_amount(int cents)
     return count;
 }
 
+// Function to calculate the number of nickels
 int nickel_amount(int cents)
 {
     int count = 0;
@@ -71,6 +85,7 @@ int nickel_amount(int cents)
     return count;
 }
 
+// Function to calculate the number of pennies
 int penny_amount(int cents)
 {
     int count = 0;
