ff3822b58e01aeb5c8ee296ec0bd2cc084a59f8b
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-21 22:39:16 +0100

    Update mario.c

--- Week 1 - C/Problem Set - 1/Solutions/mario.c
+++ Week 1 - C/Problem Set - 1/Solutions/mario.c
@@ -3,19 +3,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// Function prototypes
 int get_size(void);
 void print_grid(int n);
 
 int main(void)
 {
+    // Get the size of the pyramid from the user
     int n = get_size();
+    // Print the grid based on the size
     print_grid(n);
 }
 
-// Recieve input
+// Function to receive input for the pyramid height
 int get_size(void)
 {
     int n;
+    // Prompt user until a valid height (1 to 8) is entered
     do
     {
         n = get_int("Height: ");
@@ -24,28 +28,30 @@ int get_size(void)
     return n;
 }
 
-// Create block
+// Function to print the pyramid grid
 void print_grid(int n)
 {
-    // Column
+    // Loop for each row
     for (int i = 0; i < n; i++)
     {
-        // Row
+        // Print leading spaces
         for (int k = 1; k < (n - i); k++)
         {
             printf(" ");
         }
+        // Print left side of the pyramid
         for (int j = 0; j < (i + 1); j++)
         {
             printf("#");
         }
+        // Print gap between the two sides of the pyramid
         printf("  ");
+        // Print right side of the pyramid
         for (int j = 0; j < (i + 1); j++)
         {
             printf("#");
         }
-
-
+        // Move to the next line after printing one row
         printf("\n");
     }
 }
