9965d5a9562038618f88ea75b367b88f5e8e0e7c
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-22 20:27:52 +0100

    Update 01.scrabble.c

--- Week 2 - Arrays/Problem set - 2/01.scrabble.c
+++ Week 2 - Arrays/Problem set - 2/01.scrabble.c
@@ -1,49 +1,63 @@
+// This program simulates a game of Scrabble between two players. Each player enters a word, and the program calculates the score of each word based on Scrabble letter values. The player with the higher score wins.
+
+// Include necessary libraries
 #include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
-int POINTS[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,1};
+// Array representing the point value of each letter in the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 1};
 
+// Function prototype
 int compute_score(string word);
 
 int main(void)
 {
+    // Get input words from both players
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
+    // Compute the scores for both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    if(score1 > score2)
+    // Determine the winner
+    if (score1 > score2)
     {
         printf("Player 1 wins!\n");
     }
     else if (score1 < score2)
     {
         printf("Player 2 wins!\n");
-
     }
     else
     {
         printf("Tie!\n");
-
     }
 }
 
+// Function to compute the score of a given word
 int compute_score(string word)
 {
     int score = 0;
-    for(int i = 0; i < strlen(word); i++){
-        if(isupper(word[i]))
+
+    // Iterate over each character in the word
+    for (int i = 0; i < strlen(word); i++)
+    {
+        // Check if the character is an uppercase letter
+        if (isupper(word[i]))
         {
-            score += POINTS[word[i]- 'A'];
+            // Add corresponding points for uppercase letter
+            score += POINTS[word[i] - 'A'];
         }
-        else if(islower(word[i]))
+        // Check if the character is a lowercase letter
+        else if (islower(word[i]))
         {
+            // Add corresponding points for lowercase letter
             score += POINTS[word[i] - 'a'];
-        };
-    };
+        }
+    }
 
     return score;
 }
