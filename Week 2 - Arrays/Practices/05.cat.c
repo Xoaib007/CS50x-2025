72edf5adf8e4039a3195e4c407d4cb0b804def06
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-23 02:59:31 +0100

    Create 05.cat.c

---
+++ Week 2 - Arrays/Practices/05.cat.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void meow(int n);
+
+int main(void)
+{
+  int n;
+  do
+  {
+    n = get_int("Number: ");
+    if (n < 1)
+    {
+        printf("Invalid input. Please write an positive number.\n");
+    }
+  }
+  while (n < 1);
+
+  meow(n);
+}
+
+void meow(int times)
+{
+  for (int i = 0; i < times; i++)
+  {
+    printf("meow\n");
+  }
+}
