1b72c55fffdf48fa88e531ada40d5df64b374930
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-23 03:29:12 +0100

    Update 05.contacts.c

--- Week 2 - Arrays/Practices/05.contacts.c
+++ Week 2 - Arrays/Practices/05.contacts.c
@@ -1,47 +1,11 @@
-#include <stdio.h>
 #include <cs50.h>
-#include <string.h>
-
-typedef struct
-{
-    string name;
-    string number;
-    string email;
-}
-person;
+#include <stdio.h>
 
 int main(void)
 {
-    // person people[3];
+    string name = get_string("Name: ");
+    long number = get_long("Number: ");
+    int age = get_int("Age: ");
 
-    // people[0].name = "John";
-    // people[1].name = "John";
-    // people[2].name = "John";
-
-    // people[0] = {"John", "39-3486925084", "john@email.com"};
-    // people[1] = {"David","39-3486925085", "david@email.com" };
-    // people[2] = {"Muller", "39-3486925086","muller@email.com" };
-
-    person people[] = {
-        {"John", "39-3486925084", "john@email.com"},
-        {"David","39-3486925085", "david@email.com" },
-        {"Muller", "39-3486925086","muller@email.com" }
-    };
-
-    string name[] = {"John", "David", "Muller"};
-    string number[] = {"39-3486925084","39-3486925085","39-3486925086"};
-    string email[] = {"john@email.com","david@email.com","muller@email.com"};
-
-    string searchName = get_string("Search name: ");
-
-    for(int i = 0 ; i < 3; i++)
-    {
-        if(strcmp(people[i].name,searchName) == 0)
-        {
-            printf("Number: %s \nEmail: %s \n", people[i].number, people[i].email);
-            return 0;
-        }
-    }
-    printf("Sorry, this person doesnt exist in your contact-book. \n");
-
+    printf("Are you sure you want to submit\nName: %s\nNumber: %li\nAge: %i", name, number, age);
 }
