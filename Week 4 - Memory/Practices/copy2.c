a36d53a4b5eb02fccbf60cccb6c14b9876fe1589
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-04-05 11:16:20 +0200

    Update copy2.c

--- Week 4 - Memory/Practices/copy2.c
+++ Week 4 - Memory/Practices/copy2.c
@@ -2,22 +2,29 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h> // to use malloc
 
 int main(void)
 {
     // Get two strings
     char *s = get_string("s: "); // input: hi!
     char *t = malloc(strlen(s) + 1); //tells computer to allocate free spaces equal to the length of s and 1 more space for "/0"
+    if(t == NULL)
+    {
+        return 0;
+    } // return if malloc allocates memory
 
     for(int i = 0, n = strlen(t), i <= n, i++)
     {
         t[i] = s[i];
-    }
+    } // copy the s string
 
-    t[0] = toupper(t[0]);
+    if(strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    } // capitalize the copied string only if the string exists
 
-    printf("s: %s\n", s); //output: hi!
-    printf("t: %s\n", t); //output: HI!
+    printf("s: %s\n", s); //output: Hi!
+    printf("t: %s\n", t); //output: Hi!
 
 }
